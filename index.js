27848d9a8c027d128d7fc2ca335940f5cdfda228
Mahin05 <mahin.islam05@gmail.com>
2022-06-10 23:11:19 +0600

    portfolio server side

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.vygvm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const projectCollection = client.db('portfolio').collection('projects');
+
+        app.get('/projects', async (req, res) => {
+            const query = {};
+            const cursor = projectCollection.find(query);
+            const projects = await cursor.toArray();
+            res.send(projects);
+        })
+
+        app.get('/project/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const project = await projectCollection.findOne(query);
+            res.send(project);
+          })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running server!')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
