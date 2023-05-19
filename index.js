b6ffdb7e7900f6e95e9f221188d9f221631e0aaf
Mahin05 <mahin.islam05@gmail.com>
2023-05-19 18:37:39 +0600

    updated portfolio mongodb user name & password

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.vygvm.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.vygvm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://mahin:am1iFgMyUhJm4amq@cluster0.vygvm.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
